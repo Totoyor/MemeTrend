0cfa29f76e9bf6bc55032984eaeb8a5726d53482
Mathieu Paindavoine <mathieupaindavoine@MacBook-Pro-de-Mathieu-Paindavoine.local>
2017-04-12 14:50:09 +0200

    Ajout des couleurs, des jeux et une image

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+
+/* Headroom.js */
+// grab an element
+var myElement = document.querySelector("header");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init(); 
